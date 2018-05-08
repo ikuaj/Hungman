bd8cf16cdf6bca696b5e12118ce773bb1e67f5e6
Kasuku <kasuku@localhost.localdomain>
2018-05-08 10:43:35 -0400

    Select function working

--- src/main/java/Hungman.java
+++ src/main/java/Hungman.java
@@ -2,21 +2,30 @@ import java.io.*;
 import java.util.Random;
 
 public class Hungman {
+
+    private char[] word;
+
     public static char[] star;
         public static void main (String args[])
         {   
             Hungman randomWord = new Hungman();
             randomWord.selectCountry();
+            randomWord.playHungman();
 
         
     }
 
     public void selectCountry(){
         Random r = new Random();
-        String[] country = { "pakistan", "india", "jarmany", "america", "rashia", "iran", "iraq", "japan", "sudan", "canada"};
+        String[] country = { "pakistan", "india", "germany", "america", "russia", "iran", "iraq", "japan", "sudan", "canada"};
             int x = r.nextInt(country.length);
             String countrySelected = country[x];
-            char[] word = countrySelected.toCharArray();
-            System.out.println("Start Game");
+            word = countrySelected.toCharArray();
+            System.out.println("A random country has been selected. Guess the country:");
+    }
+
+    public void playHungman(){
+        int length = word.length;
+        System.out.println("CLUE: The word length is " + length + " characters long.");
     }
 }
